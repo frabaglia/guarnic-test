8c0232864607cedb4ea8d9c71329c745aaff512e
Franco Rabaglia <francoo.hd@gmail.com>
2021-04-17 14:51:03 -0300

    Adding runnable after 30 days script.

---
+++ after-30-days.js
@@ -0,0 +1,25 @@
+const { Product, CarInsurance } = require('./src/coTest');
+
+const productsAtDayZero = [
+    new Product('Medium Coverage', 10, 20),
+    new Product('Full Coverage', 2, 0),
+    new Product('Low Coverage', 5, 7),
+    new Product('Mega Coverage', 0, 80),
+    new Product('Mega Coverage', -1, 80),
+    new Product('Special Full Coverage', 15, 20),
+    new Product('Special Full Coverage', 10, 49),
+    new Product('Special Full Coverage', 5, 49),
+    new Product('Super Sale', 3, 6),
+];
+
+const carInsurance = new CarInsurance(productsAtDayZero);
+const productPrinter = function (product) {
+    console.log(`${product.name}, ${product.sellIn}, ${product.price}`);
+};
+
+for (let i = 1; i <= 30; i += 1) {
+    console.log(`Day ${i}`);
+    console.log('name, sellIn, price');
+    carInsurance.updatePrice().forEach(productPrinter);
+    console.log('');
+}
